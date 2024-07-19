b3c6cae9e726782d1fef083dbc0566bebd8741de
Amity808 <bolarinwamuhdsodiq0@gmail.com>
2024-07-19 23:00:24 +0100

    fix bug

--- waste-insured/src/components/HospitalFilter.jsx
+++ waste-insured/src/components/HospitalFilter.jsx
@@ -5,7 +5,7 @@ import React from "react"
     return (
       <div className=" flex justify-center ">
         <input type="text" className="border-[#EFAE07] border-2 rounded-3xl py-2 px-5" onChange={(e) => onSearch(e.target.value)} placeholder="Search Waste Type" name="searchBar" id="searchBar" />
-        {/* <button>Search</button> */}
+        
       </div>
     )
   }
